fd9747366bcd13a0fd8feac2283f92570f4f79be
huythanh111982 <thanhhuy@kiara.local>
2020-12-25 00:56:01 +0700

    ref #4 Display categories data in app

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="column is-one-quarter"
+    :style="{ 'min-height': '160px' }"
+  >
+    <a href="#">
+      <span class="is-primary is-top is-medium tooltip">
+        <figure class="image is-4by3 imageFade">
+          <img class="is-rounded" :src="category.image" />
+        </figure>
+        <div class="subtitle m-t-xs bold">{{ category.name  | captilize}}</div>
+      </span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    category: {
+      type: Object,
+      required:true
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.is-rounded {
+  border-radius: 10px !important;
+}
+</style>
