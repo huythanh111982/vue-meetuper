87238f89dfd27e5d7ef3495719d7feac3136a601
huythanh111982 <thanhhuy@kiara.local>
2020-12-27 23:40:30 +0700

    Add Page  Not Found

---
+++ src/components/ErrorView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="notFoundContainer">
+      <div class="m-b-xxl">
+        <h1 class="title">{{title}}</h1>
+        <router-link :to="{name: navigateToPage}" class="button is-primary">{{navigaToText}}</router-link>
+      </div>
+      <h2 class="subtitle">{{status}}<span> :(</span></h2>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+     title: {
+       type: String,
+       reuired:true 
+     },
+     status: {
+       type: String,
+       reuired:true 
+     },
+     navigateToPage: {
+       type: String,
+       reuired:true 
+     },
+     navigaToText: {
+       type: String,
+       reuired:true 
+     },
+   },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+      <ErrorView :title="'Ooooops, page you are trying to access doesn\'t exist'" 
+      :status="404" 
+      :avigateToPage ="'PageHome'" 
+      :navigateToText ="'Navigate back Home'"/>
+  </div>
+</template>
+
+<script>
+import ErrorView from '@/components/ErrorView'
+  export default {
+   components: {
+     ErrorView,
+   },
+  }
+</script>
+
+<style scoped>
+  .title {
+    font-size: 40px;
+  }
+  .subtitle {
+    font-size: 140px;
+    text-align: center;
+  }
+  .notFoundContainer {
+    margin-top: 80px;
+  }
+</style>
+ 
